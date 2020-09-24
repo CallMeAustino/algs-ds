9196760ace79fb3cd8edbeaca433f9992d72e224
Austin <austin.hokun.wong@gmail.com>
2020-09-24 19:10:53 -0400

    added perms

--- answers.js
+++ answers.js
@@ -28,11 +28,11 @@ var restoreString = function (s, indices) {
 // font awesome icons are a stored svg that cannot contain html elements
 // create a div component next to svg icon to get hoverable icon effect
 
-var reverse = function (x) {
-    let x_array = x.split("");
-};
+// var reverse = function (x) {
+//     let x_array = x.split("");
+// };
 
-reverse(123);
+// reverse(123);
 
 /*Cracking the Coding Interview*/
 /*Arrays and Strings*/
@@ -51,4 +51,37 @@ reverse(123);
         return unique;
     }
 
+/*First check the .size property on both maps. If the two maps don't have the same number of keys, then you know right away, they can't be identical.
+Furthermore, guaranteeing that they have the same number of keys allows you to just iterate one of the maps and compare its values to the other.
+Use the for (var [key, val] of map1) iterator syntax for iterating the keys so you don't have to build or sort an array of keys yourself (should be both faster and more memory efficient).
+Then, lastly, if you make sure that the comparison returns immediately as soon as a mismatch is found, then it will shorten the execution time when they are not the same.*/
+
+    var isPermutation = function(string1, string2) {
+        let table1 = {};
+        let table2 = {};
+        if (string1.length != string2.length){
+            return false;
+        }
+        for(idx=0;idx<string1.length;idx++){
+            if(table1[string1[idx]] === undefined){
+                table1[string1[idx]] = 0;
+            } else {
+                table1[string1[idx]] += 1;
+            }
+            if (table2[string2[idx]] === undefined) {
+                table2[string2[idx]] = 0;
+            } else {
+                table2[string2[idx]] += 1;
+            }
+        }
+
+        if(table1.size !== table2.size){
+            return false;
+        }
+
+        Object.keys(table1);
+    }
+
+
+    isPermutation("hello","olleh");
 
